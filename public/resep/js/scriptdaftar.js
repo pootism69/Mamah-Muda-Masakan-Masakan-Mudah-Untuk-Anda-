f442eea600845f54f4f51ac4825202d4bf820d04
Fawwaz Yasirah <fawwazyasirah@gmail.com>
2024-06-04 14:40:24 +0700

    first-api-branch-commit

--- public/resep/js/scriptdaftar.js
+++
@@ -1,5 +0,0 @@
-    // Fungsi untuk tombol "Daftar"
-    document.getElementById("daftarBtn").addEventListener("click", function() {
-        // Mengarahkan ke halaman "halaman-daftar.html"
-        window.location.href = "login.html";
-    });
